1f638a11efd3ddbf65dd14cd181bbdca4b50969c
nguyenthanhoang <nthoang0708@gmail.com>
2025-04-05 22:52:55 +0700

    Tich hop Swagger UI de tao tai lieu API voi Django REST Framework

--- courseapis/courseapis/urls.py
+++ courseapis/courseapis/urls.py
@@ -18,8 +18,29 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from courses.admin import admin_site
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Tạo Schema View cho Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Course API",
+        default_version='v1',
+        description="APIs for CourseApp",
+        contact=openapi.Contact(email="2251050031hoang@ou.edu.vn"),
+        license=openapi.License(name="Nguyễn Thanh Hoàng@2025"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# Cấu hình urlpatterns
 urlpatterns = [
     path('', include('courses.urls')),
     path('admin/', admin_site.urls),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name = 'schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
